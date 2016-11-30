b33b1fc8e372d121fb26c9ac8e9639f76ecb40fc
unknown <lisubin@we.com>
2016-11-30 15:42:33 +0800

    add

---
+++ user-web/src/test/java/com/abin/lee/test/MapTest.java
@@ -0,0 +1,17 @@
+package com.abin.lee.test;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Created by abin
+ * Be Created in 2016/11/30.
+ */
+public class MapTest {
+    public static void main(String[] args) {
+        Map<String, String> request = Maps.newHashMap();
+
+//        request.
+    }
+}
